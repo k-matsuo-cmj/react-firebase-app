527a61d741ab0e0c4b037c5d3e9ecc24c086df8c
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-02-12 12:51:20 +0900

    @material-ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Button, Avatar, Typography } from '@material-ui/core';
+
+const styles = theme => ({
+  root: { flexGrow: 1, },
+  flex: { flexGrow: 1, },
+  button: { margin: theme.spacing() },
+});
 
 class Header extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>Header is here</div>
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.flex}>
+              Firebase video
+            </Typography>
+            <Button color="inherit" className={classes.button} onClick={this.googleLogin}>
+              Login with Google
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
-export default Header;
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+export default withStyles(styles)(Header);
